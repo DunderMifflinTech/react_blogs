8bb945d0d0493565438a9b9c80ba4200f3eb78bb
Prateek Rohilla <theprateekkukreja@gmail.com>
2022-07-16 22:09:24 +0530

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      monospace: ['Source Code Pro', 'monospace'],
+    },
+  },
+  plugins: [],
+};
