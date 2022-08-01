d86a5ca16d3022511bd880f97a4aa14c1c0cf30c
karikey rana <rana.kartikey999@gmail.com>
2022-08-01 15:13:04 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 import React from 'react';
-import { Routes, Route, Link, useMatch } from 'react-router-dom';
+import './index.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
+  
   return (
-    <div className="App min-h-[100vh] w-full bg-slate-100">
-      <Navbar />
+      <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route path = '/' element = {<Navbar/>} />
       </Routes>
-    </div>
+      </>
   );
 }
 
 export default App;
-
-function About() {
-  return <div>About page</div>;
-}
-function Home() {
-  return <div>Home page</div>;
-}
-function Dashboard() {
-  return <div>dashboard</div>;
-}
-
-function Navbar() {
-  return (
-    <div className="w-full flex px-4 items-center h-[60px] bg-gray-500 text-white">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="about">About</NavLink>
-      <NavLink to="dashboard">Dashboard</NavLink>
-    </div>
-  );
-}
-
-function NavLink({ to = '/', className = '', children }) {
-  const isSelected = useMatch(to);
-  return (
-    <Link
-      to={to}
-      className={`${
-        isSelected ? `font-bold` : `font-semibold`
-      } text-[20px] hover:bg-gray-300 focus:outline-2 focus:outline-cyan-500 hover:text-black rounded-lg transition-all px-2 py-1 ${className}`}
-    >
-      {children}
-    </Link>
-  );
-}
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+function navbar() {
+    return (
+        <div>
+            <nav className='navbar w-full h-20 border-y-2 flex justify-between items-center'>
+                <ul className = 'cursor-pointer flex'>
+                    <li className=' user-name font-bold list-none'>Name</li>
+                    <li className='user-name-blog list-none co'>.Blog</li>
+                </ul>
+                <ul className='flex justify-end pr-8'>
+                    <li className = 'pr-10'><NavLink to='/'>Home</NavLink></li>
+                    <li className = 'pr-10'><NavLink to='/'>My Articles</NavLink></li>
+                    <li className = 'pr-10'><NavLink to='/'>Search</NavLink></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default navbar
