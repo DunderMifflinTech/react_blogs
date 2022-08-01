3b2e3f123cb63e5e732e14a4343c127887b379dd
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2022-08-01 16:52:22 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
-  
   return (
-      <>
+    <>
+      <Navbar />
       <Routes>
-        <Route path = '/' element = {<Navbar/>} />
+        <Route path="/" element={<button />} />
+
       </Routes>
-      </>
+    </>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './Navbar.css'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
 
-function navbar() {
-    return (
-        <div>
-            <nav className='navbar w-full h-20 border-y-2 flex justify-between items-center'>
-                <ul className = 'cursor-pointer flex'>
-                    <li className=' user-name font-bold list-none'>Name</li>
-                    <li className='user-name-blog list-none co'>.Blog</li>
-                </ul>
-                <ul className='flex justify-end pr-8'>
-                    <li className = 'pr-10'><NavLink to='/'>Home</NavLink></li>
-                    <li className = 'pr-10'><NavLink to='/'>My Articles</NavLink></li>
-                    <li className = 'pr-10'><NavLink to='/'>Search</NavLink></li>
-                </ul>
-            </nav>
-        </div>
-    )
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar w-full h-20 border-y-2 flex justify-between items-center">
+        <ul className="cursor-pointer flex">
+          <li className=" user-name font-bold list-none">Name</li>
+          <li className="user-name-blog list-none co">.Blog</li>
+        </ul>
+        <ul className="flex justify-end pr-8">
+          <li className="pr-10">
+            <NavLink to="/home">Home</NavLink>
+          </li>
+          <li className="pr-10">
+            <Link to="/article">My Articles</Link>
+          </li>
+          <li className="pr-10">
+            <NavLink to="/search">Search</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default navbar
+export default Navbar;
