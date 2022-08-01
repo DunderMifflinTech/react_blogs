537a02208c2a2770bc565736d6734214bc51bde0
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2022-08-01 21:11:39 +0530

    added basic components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Playground from './pages/Playground/Playground';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<button />} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/p" element={<Playground />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,7 +7,9 @@ function Navbar() {
     <div>
       <nav className="navbar w-full h-20 border-y-2 flex justify-between items-center">
         <ul className="cursor-pointer flex">
-          <li className=" user-name font-bold list-none">Name</li>
+          <NavLink to="/">
+            <li className=" user-name font-bold list-none">Name</li>
+          </NavLink>
           <li className="user-name-blog list-none co">.Blog</li>
         </ul>
         <ul className="flex justify-end pr-8">
