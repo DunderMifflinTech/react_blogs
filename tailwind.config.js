8f72ab0dc8c14905b2ac56d8182f772599794464
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2022-08-03 13:53:30 +0530

    fix for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
-    fontFamily: {
-      monospace: ['Source Code Pro', 'monospace'],
-    },
   },
   plugins: [],
 };
