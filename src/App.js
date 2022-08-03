31523c0a59a81e35549ff95e143e546f2fe3b6e3
karikey rana <rana.kartikey999@gmail.com>
2022-08-03 11:35:47 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
--- src/components/ArticleRecommendations/ArticleRecommendations.js
+++ src/components/ArticleRecommendations/ArticleRecommendations.js
@@ -1,25 +1,35 @@
-import React from 'react'
-import BlogPost from '../BlogPost/BlogPost'
+import React from 'react';
+import BlogPost from '../BlogPost/BlogPost';
 import laptop from '../../images/laptop.png';
 import userPFP from '../../images/userPFP.png';
 import BaseButton from '../BaseButton/BaseButton';
 
 function ArticleRecommendations({ section }) {
-    return (
-        <div className='w-full flex flex-col bg-white min-w-[300px]'>
-            <div className = 'w-full flex flex-row flex-wrap px-[90px] py-[20px] justify-between'>
-                <div className = 'font-[700] flex items-center justify-center'>{section}</div>
-                <BaseButton className='font-[700]' variant = 'ghost'>See all  articles</BaseButton>
-            </div>
-
-            <div className='flex flex-row flex-wrap mx-[70px] justify-between mb-[40px]'>
-                <BlogPost title='title here' creatorName='CreatorName' image={laptop} dateCreated={new Date()} creatorPFP={userPFP} />
-                <BlogPost title='title here' creatorName='CreatorName' image={laptop} dateCreated={new Date()} creatorPFP={userPFP} />
-                <BlogPost title='title here' creatorName='CreatorName' image={laptop} dateCreated={new Date()} creatorPFP={userPFP} />
-                <BlogPost title='title here' creatorName='CreatorName' image={laptop} dateCreated={new Date()} creatorPFP={userPFP} />
-            </div>
+  var arr = [1, 2, 3, 4];
+  return (
+    <div className="w-full flex flex-col bg-white min-w-[300px]">
+      <div className="w-full flex flex-row flex-wrap px-[90px] py-[20px] justify-between">
+        <div className="font-[700] flex items-center justify-center">
+          {section}
         </div>
-    )
+        <BaseButton className="font-[700]" variant="ghost">
+          See all articles
+        </BaseButton>
+      </div>
+
+      <div className="flex flex-row flex-wrap mx-[70px] justify-between mb-[40px]">
+        {arr.map((val, idx) => (
+          <BlogPost
+            creatorName="CreatorName"
+            image={laptop}
+            dateCreated={new Date()}
+            creatorPFP={userPFP}
+            title="title here How to create a full stack web app in 2022"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ArticleRecommendations;
