c39563bef02c0f80091d44638e1e36625b98ff5d
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2023-09-13 19:22:35 +0200

    clean up main

--- src/components/userWall/children/topicWisearticle/TopicWiseArticle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TopicWiseArticle() {
-  return <div className="col-start-3 col-end-4 border-2 border-black ">TopicWiseArticle</div>;
-}
-
-export default TopicWiseArticle;
--- src/pages/home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ArticleRecommendations from '../../components/ArticleRecommendations/ArticleRecommendations';
-
-function Home() {
-  return <ArticleRecommendations section="Articles you may like" />;
-}
-
-export default Home;
