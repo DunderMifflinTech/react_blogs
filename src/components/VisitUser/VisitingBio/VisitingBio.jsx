f3b039e2f94d7474cc5acd0b613f9203694668e0
karikey rana <rana.kartikey999@gmail.com>
2023-09-21 15:05:37 +0530

    bg upload button

--- src/components/VisitUser/VisitingBio/VisitingBio.jsx
+++ src/components/VisitUser/VisitingBio/VisitingBio.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react';
 import { BsFillCameraFill } from 'react-icons/bs';
 import unknownPerson from '../../../images/UnknownPerson.jpg';
+import backgroundPlaceholder from '../../../images/backgroundImage.png';
 import nature from '../../../images/nature.png';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,17 +11,28 @@ import BaseButton from '../../BaseButton/BaseButton';
 function VisitingBio({ ...restOfProps }) {
   const visitingUser = useSelector((state) => state.visitingUser);
   const auth = useSelector((state) => state.auth);
+  const[bghover, setBghover] = useState(false);
+
+  const openBGImageModal = () =>{
+
+  }
 
   return (
     <div className=" w-full flex flex-col items-center bg-[#ffff] max-w-[4000px] min-w-[240px]">
       <div className=" w-[85%] relative overflow-y-visible user-img h-full grid items-center">
-        <img
-          className="w-full h-[calc(35vw-10px)] object-cover rounded-b-2xl"
-          src={visitingUser?.backgroundPictureURL || nature}
-        ></img>
+        <div className="bg-img-cont relative transition-all cursor-pointer"
+        onClick={openBGImageModal}
+        onMouseEnter={()=>setBghover(true)}
+        onMouseLeave={()=>setBghover(false)}>
+          <img
+            className=" w-full h-[calc(28vw-10px)] object-cover rounded-b-2xl xsm:h-[calc(34vw-10px)]"
+            src={visitingUser?.backgroundPictureURL || backgroundPlaceholder}
+          ></img>
+          <div className='background-blur absolute w-full rounded-b-2xl bottom-0 h-[100px] pt-[50px] text-center text-white fi'>{bghover && 'Upload Image'}</div>
+        </div>
         <img
           src={visitingUser?.user?.profilePictureURL || unknownPerson}
-          className=" absolute h-[160px] w-[160px] sm:h-[100px] sm:w-[100px] border-[#ffffff] border-4 rounded-full object-cover top-[calc(32vw-10px)] left-[calc(10%-50px)] sm:left-[calc(50%-50px)] sm:top-[calc(25vw-10px)] xl:top-[calc(30vw-10px)] xl:left-[calc(10%-50px)]"
+          className=" absolute h-[160px] w-[160px] sm:h-[100px] sm:w-[100px] border-[#ffffff] border-4 rounded-full object-cover top-[calc(24.5vw-10px)] left-[calc(10%-50px)] sm:left-[calc(50%-50px)] sm:top-[calc(19vw-10px)] xl:top-[calc(25vw-10px)] xl:left-[calc(10%-50px)]"
         ></img>
         <div className="pt-[10px] flex flex-row sm:mt-[80px]">
           <div className="xl:w-[220px] 2xl:w-[260px] 3xl:w-[300px] 4xl:w-[350px] 5xl:w-[700px] sm:w-0"></div>
@@ -45,7 +57,7 @@ function VisitingBio({ ...restOfProps }) {
           </div> */}
         </div>
       </div>
-      <hr className="mb-[20px] w-[85vw] "></hr>
+      <hr className="my-[40px] w-[85vw] "></hr>
     </div>
   );
 }
